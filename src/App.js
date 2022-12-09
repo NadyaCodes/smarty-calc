4f449043d47dd28bb18e83586d4e4234dffe0096
NadyaCodes <nadyacorscadden@gmail.com>
2022-12-09 16:08:29 -0600

    add emoji intro

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Calculator from "./Calculator";
 function App() {
   return (
     <div className="App">
-      <h1>Smarty Calc</h1>
+      <div className="header">
+        <h1>Smarty Calc</h1>
+        <div className="bouncy">🤓</div>
+      </div>
+
       <Calculator />
     </div>
   );
