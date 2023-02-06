b45d42f3fafca177bdce2b36d0d84d007d9b035a
NadyaCodes <nadyacorscadden@gmail.com>
2023-02-06 09:30:23 -0500

    floating point math fix attempt

--- src/helpers.js
+++ src/helpers.js
@@ -7,13 +7,54 @@ const mathFunc = (symbol, current, next) => {
   const secondNum = parseFloat(next);
   switch (symbol) {
     case "\u002B":
-      return firstNum + secondNum;
+      let addedAnswer = (firstNum + secondNum).toFixed(10);
+      while (
+        addedAnswer[addedAnswer.length - 1] === "0" &&
+        addedAnswer[addedAnswer.length - 1] !== "."
+      ) {
+        addedAnswer = addedAnswer.slice(0, -1);
+      }
+      if (addedAnswer[addedAnswer.length - 1] === ".") {
+        addedAnswer = addedAnswer.slice(0, -1);
+      }
+      return addedAnswer;
+
     case "\u2212":
-      return firstNum - secondNum;
+      let subAnswer = (firstNum - secondNum).toFixed(10);
+      while (
+        subAnswer[subAnswer.length - 1] === "0" &&
+        subAnswer[subAnswer.length - 1] !== "."
+      ) {
+        subAnswer = subAnswer.slice(0, -1);
+      }
+      if (subAnswer[subAnswer.length - 1] === ".") {
+        subAnswer = subAnswer.slice(0, -1);
+      }
+      return subAnswer;
     case "\u2715":
-      return firstNum * secondNum;
+      let multAnswer = (firstNum * secondNum).toFixed(10);
+      while (
+        multAnswer[multAnswer.length - 1] === "0" &&
+        multAnswer[multAnswer.length - 1] !== "."
+      ) {
+        multAnswer = multAnswer.slice(0, -1);
+      }
+      if (multAnswer[multAnswer.length - 1] === ".") {
+        multAnswer = multAnswer.slice(0, -1);
+      }
+      return multAnswer;
     case "\u00F7":
-      return firstNum / secondNum;
+      let divAnswer = (firstNum / secondNum).toFixed(10);
+      while (
+        divAnswer[divAnswer.length - 1] === "0" &&
+        divAnswer[divAnswer.length - 1] !== "."
+      ) {
+        divAnswer = divAnswer.slice(0, -1);
+      }
+      if (divAnswer[divAnswer.length - 1] === ".") {
+        divAnswer = divAnswer.slice(0, -1);
+      }
+      return divAnswer;
     default:
       return;
   }
